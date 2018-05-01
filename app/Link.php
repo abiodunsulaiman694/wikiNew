c71c7dace541437595cd297a6683d9181cd5a540
BRYTA <abiodunsulaiman694@gmail.com>
2018-05-02 00:28:56 +0100

    Configured database models for fillables and relationship functions

--- app/Link.php
+++ app/Link.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Link extends Model
 {
-    //
+  use SoftDeletes;
+
+  function page() {
+    return $this->belongsTo(Page::class, 'page_id');
+  }
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'link_title', 'link_url', 'exiistent'
+  ];
 }
--- app/Page.php
+++ app/Page.php
@@ -3,8 +3,35 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Page extends Model
 {
-    //
+  use SoftDeletes;
+
+  function links() {
+    return $this->hasMany(Link::class, 'page_id');
+  }
+
+  function redirect() {
+    return $this->belongsTo(Page::class, 'redirect_id');
+  }
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'redirect_id'
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+
+  ];
 }
