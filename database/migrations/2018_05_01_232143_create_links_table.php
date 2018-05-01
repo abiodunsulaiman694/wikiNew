1ff9c192ff46113ea60e655ebaf2d49cb48bc3b2
BRYTA <abiodunsulaiman694@gmail.com>
2018-05-02 00:27:04 +0100

    Created database migrations and models. Migrated tables to database

---
+++ database/migrations/2018_05_01_232143_create_links_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+          $table->increments('id');
+          $table->integer('page_id');
+          $table->string('link_title');
+          $table->string('link_url')->nullable();
+          $table->integer('existent')->default(0);
+          $table->timestamps();
+          $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
